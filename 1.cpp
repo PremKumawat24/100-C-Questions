fe00a80398830643c3d843322af75548df073333
Prem Kumawat <premkumavat2024@gmail.com>
2024-11-18 11:33:46 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+// Write a C++ program that prints the following text to the screen: 
+// Hello, World!
+
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
